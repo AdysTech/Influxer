528e33b4e094d2c476e40fd177446279116f5799
Ady <Ady@adystech.com>
2015-08-18 08:55:16 +0530

    Intial commit

---
+++ Influxer/EpochHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdysTech.Influxer
+{
+    internal static class EpochHelper
+    {
+        private static readonly DateTime Origin = new DateTime (1970, 1, 1);
+
+        public static long ToEpochMs(this DateTime time)
+        {
+            TimeSpan t = time - Origin;
+            return (long) ( t.TotalSeconds * 1000 );
+        }
+
+        public static long ToEpoch(this DateTime time)
+        {
+            TimeSpan t = time - Origin;
+            return (long) ( t.TotalSeconds );
+        }
+    }
+}
---
+++ Influxer/PerfmonCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdysTech.Influxer
+{
+    class PerfmonCounter
+    {
+        public int ColumnIndex { get; set; }
+        public string Host { get; set; }
+        public string PerformanceObject { get; set; }
+        public string CounterName { get; set; }
+    }
+}
