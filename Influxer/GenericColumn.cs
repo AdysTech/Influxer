6c989a38d1dca0d1de54f68a32c1afafcdcf5922
Ady <Ady@adystech.com>
2015-09-25 02:49:06 +0530

    Implemented filter logic for Generic filetype, added utc offset to generic

---
+++ Influxer/GenericColumn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdysTech.Influxer
+{
+    class GenericColumn
+    {
+        public int ColumnIndex { get; set; }
+        public string ColumnHeader { get; set; }
+    }
+}
